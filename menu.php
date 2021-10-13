0cb82b77c83ff2e774a7cd29b4c85263fdf77451
papi18011998 <papaibrahima98@gmail.com>
2021-10-13 12:40:21 +0000

    Mis a jour de la version 0.0.1

---
+++ menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="accueil.php">Accueil</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="us.php">Qui sommes nous?<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="login.php">Se connecter</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="produits.php">Nos produits</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
