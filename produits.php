e395403d07bce34f02228a8560e6e2751cef0313
LOMBATECH <mballohabib93@gmail.com>
2021-10-13 13:24:09 +0000

    ajoute de fichier css

--- produits.php
+++ produits.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
+<?php include "menu.php"; ?>
     <title>NOS PRODUITS</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
